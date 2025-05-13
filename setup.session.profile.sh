ae54ec9249407bad1c7057512fa8c367d6f89b83
nnmfnwl <134507773+nnmfnwl@users.noreply.github.com>
2025-05-13 16:08:54 +0000

    add session into dexsetup
    add session privacy messenger and profiling into dexsetup.

---
+++ setup.session.profile.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+# include tools
+source "./src/tools.sh" || exit 1
+
+# default arguments
+cc_script_cfg_path_default="./src/cfg.session.sh"
+cc_firejail_profile_name_default="default"
+
+# include specific tool
+source "./src/tools.session.firejail.sh" || exit 1
+
+# showhelp is auto set by tools
+tool_session_firejail_show_help
+
+# handle arguments
+cc_script_cfg_path=
+cc_install_dir_path=
+cc_firejail_profile_name=
+
+cc_firejail_default="firejail"
+cc_firejail=${cc_firejail_default}
+cc_proxychains_default="proxychains -q"
+cc_proxychains=${cc_proxychains_default}
+
+# process command line arguments
+argc=$#
+argv=("$@")
+tool_session_firejail_process_arguments ${argc} ${argv}
+
+# start measuring time
+tool_time_start
+
+tool_variable_info cc_firejail
+tool_variable_info cc_proxychains
+tool_variable_info cc_firejail_profile_name
+
+# include main session cfg script IE: ./src/cfg.session.sh
+tool_variable_check_load_default cc_script_cfg_path cc_script_cfg_path_default
+tool_realpath cc_script_cfg_path "parameter #1 session cfg script path"
+tool_check_version_and_include_script ${cc_script_cfg_path} "loading session cfg script" 
+
+# check included "./src/cfg.session.sh" cfg variables
+tool_variable_check_load_default cc_install_dir_path_default "" "session default install dir path"
+
+# prepare install directory path
+cc_install_dir_path_default_tmp=${cc_dexsetup_dir_path}"/../"${cc_install_dir_path_default}
+tool_variable_check_load_default cc_install_dir_path cc_install_dir_path_default_tmp
+tool_realpath cc_install_dir_path "cc install dir path"
+tool_make_and_check_dir_path cc_install_dir_path "cc install dir path"
+
+# firejail session profile name
+tool_variable_check_load_default cc_firejail_profile_name cc_firejail_profile_name_default
+
+# firejail session run script file path
+cc_session_firejail_run_script_path=${cc_install_dir_path}"/firejail.session."${cc_firejail_profile_name}".sh"
+tool_realpath cc_session_firejail_run_script_path "session firejail run script"
+
+# prepare session profile data path
+cc_profile_data_path=${cc_install_dir_path}"/data/profile/"${cc_firejail_profile_name}"/"
+tool_make_and_check_dir_path cc_profile_data_path "session firejail profiles data path"
+
+# prepare bin files path
+cc_bin_dir="data/download/bin"
+cc_bin_path=${cc_install_dir_path}"/"${cc_bin_dir}
+tool_realpath cc_bin_path "binary files path"
+tool_dir_if_notexist_exit cc_bin_path "binary file path" 
+
+# firejail session run script file path
+cc_session_firejail_run_script2_filename="firejail.session."${cc_firejail_profile_name}".sh"
+cc_session_firejail_run_script2_path=${cc_bin_path}"/"${cc_session_firejail_run_script2_filename}
+tool_realpath cc_session_firejail_run_script2_path "session firejail run script"
+
+# generate firejail session run script 1
+script_data="#!/bin/bash
+
+# run script generated with ./setup.session.firejail.sh --help
+
+cd ${cc_bin_dir} || exit 1
+
+firejail \\
+--name=session.${cc_firejail_profile_name} \\
+--whitelist=\`pwd\` \\
+--read-only=\`pwd\` \\
+--whitelist=\${HOME}/.proxychains \\
+--read-only=\${HOME}/.proxychains \\
+--mkdir=${cc_profile_data_path} \\
+--whitelist=${cc_profile_data_path} \\
+--private-tmp \\
+    ./${cc_session_firejail_run_script2_filename}
+"
+tool_make_and_chmod_script_or_exit cc_session_firejail_run_script_path script_data "create session firejail run script"
+
+# generate firejail session run script 2
+script_data2="#!/bin/bash
+
+# run script generated with ./setup.cc.session.firejail.sh --help
+
+mkdir -p ~/.config/ || exit 1
+ln -s ${cc_profile_data_path} ~/.config/Session || exit 1
+
+#~ ${cc_proxychains} ./session-desktop --no-sandbox --no-zygote --disable-gpu
+
+#~ torsocks -d ./session-desktop --no-zygote --no-sandbox --disable-gpu
+
+#~ torsocks ./session-desktop --no-zygote --no-sandbox --disable-gpu
+
+#~ proxychains4 ./session-desktop --no-zygote --no-sandbox --disable-gpu
+
+proxychains4 -q ./session-desktop --no-sandbox --no-zygote --disable-gpu
+
+#~ ./session-desktop --no-zygote --no-sandbox --disable-gpu
+
+"
+
+tool_make_and_chmod_script_or_exit cc_session_firejail_run_script2_path script_data2 "create session firejail run script2"
+
+tool_time_finish_print
+
+exit 0
---
+++ src/cfg.session.sh
@@ -0,0 +1,18 @@
+cc_setup_helper_version="20210827"
+
+# path here is relative to dexsetup root directory default("~/dexsetup/dexsetup/")
+cc_install_dir_path_default="session/latest"
+cc_git_src_url="https://github.com/oxen-io/session-desktop.git"
+cc_git_src_branch_master=
+cc_git_branch="v1.15.2"
+cc_git_commit_id="073ab14a5e7fbd26d80d806e5ba73c712b75a4ab"
+cc_git_build_bin_file_dir="build"
+#~ cc_download_url="https://github.com/oxen-io/session-desktop/releases/download/v1.13.1/session-desktop-linux-amd64-1.13.1.deb"
+#~ cc_download_url="https://github.com/session-foundation/session-desktop/releases/download/v1.14.5/session-desktop-linux-amd64-1.14.5.deb"
+#~ cc_download_url="https://github.com/session-foundation/session-desktop/releases/download/v1.15.2/session-desktop-linux-amd64-1.15.2.deb"
+cc_download_url="https://github.com/session-foundation/session-desktop/releases/download/v1.16.0/session-desktop-linux-amd64-1.16.0.deb"
+cc_download_sha512sum="81f1e1c96cf48afef29b4c8c2ef2915cf370277b8b9c865aa62472851a6673b8c43ea06b2d01a8f8b8a21a02e2ae33623480f72fc6d5976a3da1525caa0bac96"
+cc_download_sha256sum="06fbf71c06f13550541b954dc0bce7bee691c982c38d0c7853f094344cea8775"
+cc_download_extracted_bin_files_dir="/opt/Session/"
+
+cc_firejail_make_args=""
