fe1700b847f5c8270a79f215920352a5869f8d7d
nnmfnwl <134507773+nnmfnwl@users.noreply.github.com>
2025-05-16 16:04:44 +0000

    Update cfg.dexbot.alfa.sh dexsetup version
    update dexbot cfg to match merge branch

--- src/cfg.dexbot.alfa.sh
+++ src/cfg.dexbot.alfa.sh
@@ -2,7 +2,7 @@ cc_setup_helper_version="20210827"
 
 cc_dexbot_install_dir_path_default="dexbot"
 cc_dexbot_git_src_url="https://github.com/nnmfnwl/dexbot.git"
-cc_dexbot_git_branch="main"
-cc_dexbot_git_commit_id="fe07f79010b15f0e7479a8fa87458625445aa833"
+cc_dexbot_git_branch="merge.2025.03.26"
+cc_dexbot_git_commit_id="f45ff41e540ea3443a1faaa94d6e77fcaa5c1623"
 
 cc_dexbot_strategy_template_path_relative="/howto/examples/bot_v2_template.py"
