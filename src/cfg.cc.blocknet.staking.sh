7342c1583973a6e3f187a58e98230604c6ef4d38
nnmfnwl <134507773+nnmfnwl@users.noreply.github.com>
2025-06-08 11:19:19 +0000

    Update cfg.cc.blocknet.staking.sh

--- src/cfg.cc.blocknet.staking.sh
+++ src/cfg.cc.blocknet.staking.sh
@@ -3,6 +3,7 @@ cc_setup_helper_version="20210827"
 source "./src/cfg.cc.blocknet.sh" || exit 1
 
 cc_chain_dir_path_default="~/.blocknet_staking"
+cc_wallet_name_default="wallet_block_dao"
 
 cc_port=41422
 cc_rpcport=41424
