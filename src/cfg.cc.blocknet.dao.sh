4dfee5cfc7ba7532978cd7cbe469ed1dec8856bf
nnmfnwl <134507773+nnmfnwl@users.noreply.github.com>
2025-06-08 11:19:04 +0000

    Update cfg.cc.blocknet.dao.sh

--- src/cfg.cc.blocknet.dao.sh
+++ src/cfg.cc.blocknet.dao.sh
@@ -3,6 +3,7 @@ cc_setup_helper_version="20210827"
 source "./src/cfg.cc.blocknet.sh" || exit 1
 
 cc_chain_dir_path_default="~/.blocknet_dao"
+cc_wallet_name_default="wallet_block_dao"
 
 cc_port=41432
 cc_rpcport=41434
