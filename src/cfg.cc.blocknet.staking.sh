b2aef2db6a151a95134e1fe1d6c321900b3a8326
nnmfnwl <134507773+nnmfnwl@users.noreply.github.com>
2025-06-20 12:40:14 +0000

    Update cfg.cc.blocknet.staking.sh - set staking and classic GUI enabled

--- src/cfg.cc.blocknet.staking.sh
+++ src/cfg.cc.blocknet.staking.sh
@@ -3,7 +3,13 @@ cc_setup_helper_version="20210827"
 source "./src/cfg.cc.blocknet.sh" || exit 1
 
 cc_chain_dir_path_default="~/.blocknet_staking"
-cc_wallet_name_default="wallet_block_dao"
+cc_wallet_name_default="wallet_block_staking"
 
 cc_port=41422
 cc_rpcport=41424
+
+# lines will eval before add
+cc_main_cfg_add='
+classic=1
+staking=1
+'${cc_main_cfg_add}
