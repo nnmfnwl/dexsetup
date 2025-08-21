2bae905c3652c060c012a4f583032acd71477c7e
no en am fro no wer lnd <134507773+nnmfnwl@users.noreply.github.com>
2025-08-21 20:38:15 +0000

    upt cfg.session.sh - update session to version 1.16.6

--- src/cfg.session.sh
+++ src/cfg.session.sh
@@ -8,9 +8,9 @@ cc_git_branch="v1.15.2"
 cc_git_commit_id="073ab14a5e7fbd26d80d806e5ba73c712b75a4ab"
 cc_git_build_bin_file_dir="build"
 
-cc_download_url_x86_64="https://github.com/session-foundation/session-desktop/releases/download/v1.16.3/session-desktop-linux-amd64-1.16.3.deb"
-cc_download_sha512sum_x86_64="0c2109f668f2ae964d1fc3b3f92410e1523dca1ade075fe55a846e45ab915854c9d95050c4353b3bf651befecd8ef8cdc749a2482c33fae4eddac45492813047"
-cc_download_sha256sum_x86_64="ffc646fec283fe685a5d61895e2a0b82411a68853dca1cdd5a2b0984678bc6ac"
+cc_download_url_x86_64="https://github.com/session-foundation/session-desktop/releases/download/v1.16.6/session-desktop-linux-amd64-1.16.6.deb"
+cc_download_sha512sum_x86_64=""
+cc_download_sha256sum_x86_64="031903847d68b9bdcf77f91e7d768b4e45f1cb41e2f2f5734fdd5d320bfce011"
 
 cc_download_extracted_bin_files_dir="/opt/Session/"
 
