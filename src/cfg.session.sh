a3509b59a204d2d6db1fcf58f79425faefa17451
no en am fro no wer lnd <134507773+nnmfnwl@users.noreply.github.com>
2025-10-03 07:56:43 +0000

    upt cfg.session.sh - update sha 512

--- src/cfg.session.sh
+++ src/cfg.session.sh
@@ -9,7 +9,7 @@ cc_git_commit_id="073ab14a5e7fbd26d80d806e5ba73c712b75a4ab"
 cc_git_build_bin_file_dir="build"
 
 cc_download_url_x86_64="https://github.com/session-foundation/session-desktop/releases/download/v1.17.0/session-desktop-linux-amd64-1.17.0.deb"
-cc_download_sha512sum_x86_64=""
+cc_download_sha512sum_x86_64="cfd0fa5c9cb8c0dfd7a9fe17bfb932cdd8ff1e6117581352c48e5d583e99de0875c2e1e2ead04a53746a2f48410906a2aba3add4fe23441edefe0ea0334aefa4"
 cc_download_sha256sum_x86_64="221a4d4cbbce3f9aa6f1d320a13b6a8745a42cec932ab19d9cacf48072532aca"
 
 cc_download_extracted_bin_files_dir="/opt/Session/"
