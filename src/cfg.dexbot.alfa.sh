d5c0169c9897cf4b13145c4feee4880ed9a32360
no en am fro no wer lnd <134507773+nnmfnwl@users.noreply.github.com>
2025-10-17 18:32:44 +0000

    upt cfg.dexbot.alfa.sh - update dexbot version

--- src/cfg.dexbot.alfa.sh
+++ src/cfg.dexbot.alfa.sh
@@ -3,6 +3,6 @@ cc_setup_helper_version="20210827"
 cc_dexbot_install_dir_path_default="dexbot"
 cc_dexbot_git_src_url="https://github.com/nnmfnwl/dexbot.git"
 cc_dexbot_git_branch="merge.2025.03.26"
-cc_dexbot_git_commit_id="5c490af735331f0d951bf0381e214bba0fa69a46"
+cc_dexbot_git_commit_id="ac839ccec9b40d8d4a90708473a49b27635a5578"
 
 cc_dexbot_strategy_template_path_relative="/howto/examples/bot_v2_template.py"
