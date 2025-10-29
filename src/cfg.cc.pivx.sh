f1b7cc75603980fb6558a38893de44078b9c5cca
no en am fro no wer lnd <134507773+nnmfnwl@users.noreply.github.com>
2025-10-29 20:16:06 +0000

    upt cfg.cc.pivx.sh - pivx build compat with ubuntu

--- src/cfg.cc.pivx.sh
+++ src/cfg.cc.pivx.sh
@@ -96,7 +96,8 @@ cd ./pivx-5.6.1/ && ./install-params.sh
 cc_port=52472
 cc_rpcport=52473
 cc_rpcuser="BlockDXPIVX"
-cc_rpcpassword=`cat /dev/urandom | tr -dc "a-zA-Z0-9" | fold -w 32 | head -n 1`
+#~ cc_rpcpassword=`cat /dev/urandom | tr -dc "a-zA-Z0-9" | fold -w 32 | head -n 1`
+cc_rpcpassword=`tr -dc A-Za-z0-9 </dev/urandom | head -c 32`
 
 # lines will eval before add
 cc_main_cfg_add='
