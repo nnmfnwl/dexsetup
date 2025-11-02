7d946e949866c000be92b970e28e3db86f8e4c89
no en am fro no wer lnd <134507773+nnmfnwl@users.noreply.github.com>
2025-11-02 18:00:54 +0000

    upt cfg.strategy.lbc.ltc.sh - update according to latest dexbot changes

--- src/cfg.strategy.lbc.ltc.sh
+++ src/cfg.strategy.lbc.ltc.sh
@@ -9,15 +9,14 @@ cc_address_funds_only="True"
 cc_address_maker_default="{cc_address_maker}"
 cc_address_taker_default="{cc_address_taker}"
 
-cc_sell_size_asset="LTC"
-# opposite strategy using "*_opposite" variables if exist
-cc_sell_size_asset_opposite="LTC"
+cc_sell_size_asset="USDT"
+cc_sell_size_asset_opposite="USDT"
 
 # automatic maker price gathering
-cc_price_redirections=' '
+cc_price_redirections='"LBC": { "asset": "USDT", "price": 0.01}'
 cc_maker_price=0
-#~ cc_price_source_argval="--usecg"
-cc_price_source_argval=" "
+cc_price_source_argval="--usecg"
+#~ cc_price_source_argval=" "
 
 # flush all canceled orders every 60 seconds
 cc_flush_canceled_orders=60
@@ -25,20 +24,20 @@ cc_flush_canceled_orders=60
 # first placed orders
 cc_sell_start_spread="2.5"
 #~ cc_sell_start_spread_opposite="1.99"
-cc_sell_start="0.025"
-cc_sell_start_min="0.0249"
+cc_sell_start="10"
+cc_sell_start_min="5"
 # last palced order
 cc_sell_end_spread="1.03"
 #~ cc_sell_end_spread_opposite="1.04"
-cc_sell_end="0.025"
-cc_sell_end_min="0.0249"
+cc_sell_end="10"
+cc_sell_end_min="5"
 
 cc_max_open_orders="3"
 
 cc_make_next_on_hit="False"
 cc_partial_orders="False"
 
-cc_reopen_finished_num=2
+cc_reopen_finished_num=1
 cc_reopen_finished_delay=300
 
 cc_reset_on_price_change_positive=0.01
@@ -48,24 +47,28 @@ cc_reset_after_delay=600
 cc_reset_after_order_finish_number=3
 cc_reset_after_order_finish_delay=0
 
-cc_boundary_asset_argval=" "
-cc_boundary_asset_track_argval=" "
-cc_boundary_reversed_pricing_argval=" "
-cc_boundary_start_price_argval=" "
-cc_boundary_max_argval=" "
-cc_boundary_min_argval=" "
+cc_sboundary_asset="''"
+cc_sboundary_max=0
+cc_sboundary_min=0
+cc_sboundary_max_track_asset="False"
+cc_sboundary_min_track_asset="False"
+cc_sboundary_max_min_reverse="True"
+cc_sboundary_max_cancel="True"
+cc_sboundary_max_exit="True"
+cc_sboundary_min_cancel="False"
+cc_sboundary_min_exit="False"
 
-#~ cc_boundary_asset_argval="--boundary_asset USDT"
-#~ cc_boundary_start_price_argval="--boundary_start_price 1"
-cc_boundary_max_argval="--boundary_max_relative 1.5"
-cc_boundary_min_argval="--boundary_min_relative 0.99"
-
-#~ cc_boundary_asset_argval="--boundary_asset USDT"
-#~ cc_boundary_asset_track_argval="--boundary_asset_track True"
-#~ cc_boundary_reversed_pricing_argval="--boundary_reversed_pricing False"
-#~ cc_boundary_reversed_pricing_argval_opposite="--boundary_reversed_pricing True"
-#~ cc_boundary_max_argval="--boundary_max_static 1.5"
-#~ cc_boundary_min_argval="--boundary_min_static 0.95"
+cc_rboundary_asset="''"
+cc_rboundary_price_initial=0
+cc_rboundary_max="0"
+cc_rboundary_min="0.9"
+cc_rboundary_max_track_asset="False"
+cc_rboundary_min_track_asset="False"
+cc_rboundary_price_reverse="False"
+cc_rboundary_max_cancel="True"
+cc_rboundary_max_exit="True"
+cc_rboundary_min_cancel="True"
+cc_rboundary_min_exit="False"
 
 cc_takerbot="0"
 
