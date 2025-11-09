5450d20cb750dd07d321fb8dbe301be801567f71
no en am fro no wer lnd <134507773+nnmfnwl@users.noreply.github.com>
2025-11-09 13:00:17 +0000

    add cfg.cc.defaults.sh - add cc_cpu_cores_reserve and cc_cpu_jobs_min for optimal build CPU usage

---
+++ src/cfg.cc.defaults.sh
@@ -0,0 +1,4 @@
+# dexsetup try to reserve 2 CPU cores for other system resources
+cc_cpu_cores_reserve=2
+# even if CPU has only 2 CPU cores which supposed to be all reserved by above variable, it use them both because minimal is 2
+cc_cpu_jobs_min=2
