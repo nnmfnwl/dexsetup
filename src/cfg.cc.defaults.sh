3bf4071fceb168cd7e40302a622f0aeedd96e4ba
no en am fro no wer lnd <134507773+nnmfnwl@users.noreply.github.com>
2025-11-15 22:54:32 +0000

    upt cfg.cc.defaults.sh - decrease reserved cores to 1

--- src/cfg.cc.defaults.sh
+++ src/cfg.cc.defaults.sh
@@ -1,4 +1,4 @@
-# dexsetup try to reserve 2 CPU cores for other system resources
-cc_cpu_cores_reserve=2
-# even if CPU has only 2 CPU cores which supposed to be all reserved by above variable, it use them both because minimal is 2
+# dexsetup try to reserve 1 CPU cores for other system resources
+cc_cpu_cores_reserve=1
+# even if CPU has only 1 CPU core which supposed to be all reserved by above variable, it creates 2 jobs because minimal is 2
 cc_cpu_jobs_min=2
