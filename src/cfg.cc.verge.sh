dc03a4881b55c63a8be6460e6ec57b1250a04cdc
no en am fro no wer lnd <134507773+nnmfnwl@users.noreply.github.com>
2025-11-16 19:03:47 +0000

    fix cfg.cc.verge.sh - fix deb 12,13 ubu 24,25

--- src/cfg.cc.verge.sh
+++ src/cfg.cc.verge.sh
@@ -32,6 +32,12 @@ stradd="\$(package)_cflags+=-Wno-error=implicit-function-declaration" &&
 '
 
 cc_make_depends="bdb"
+cc_command_configure_debian12="${cc_make_depends}"
+cc_command_configure_debian13="${cc_make_depends}"
+
+cc_command_configure_ubuntu="bdb boost"
+cc_command_configure_ubuntu24="${cc_make_depends}"
+cc_command_configure_ubuntu25="${cc_make_depends}"
 
 cc_command_configure='
 ./configure
@@ -42,6 +48,22 @@ CXXFLAGS="-O3 -march=native"
 --enable-reduce-exports --without-miniupnpc
 --with-gui=auto
 '
+cc_command_configure_debian12="${cc_command_configure}"
+cc_command_configure_debian13="${cc_command_configure}"
+
+cc_command_configure_ubuntu='
+./configure
+LDFLAGS="-L`pwd`/depends/${cc_archdir}/lib/"
+CPPFLAGS="-I`pwd`/depends/${cc_archdir}/include/"
+CXXFLAGS="-O3 -march=native"
+--with-boost-libdir=`pwd`/depends/${cc_archdir}/lib/
+--disable-bench --disable-gui-tests --disable-tests --without-tests
+--enable-reduce-exports --without-miniupnpc
+--with-gui=auto
+'
+cc_command_configure_ubuntu24="${cc_command_configure_ubuntu}"
+cc_command_configure_ubuntu25="${cc_command_configure_ubuntu}"
+
 
 # HINT >> add to above configure parameter to compile with debug symbols >>
 # --enable-debug
