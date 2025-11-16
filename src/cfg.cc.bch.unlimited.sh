a360f24663cf12ceb604e92f24d42d385dc97cf2
no en am fro no wer lnd <134507773+nnmfnwl@users.noreply.github.com>
2025-11-16 07:45:51 +0000

    fix cfg.cc.bch.unlimited.sh deb 12,13 ubu 24,25

--- src/cfg.cc.bch.unlimited.sh
+++ src/cfg.cc.bch.unlimited.sh
@@ -50,7 +50,10 @@ stradd="\$(package)_cflags+=-Wno-error=implicit-function-declaration" &&
 '
 
 cc_make_depends="bdb"
-cc_make_depends_ubuntu="bdb boost"
+cc_make_depends_debian12="${cc_make_depends}"
+cc_make_depends_debian13="${cc_make_depends}"
+cc_make_depends_ubuntu24="${cc_make_depends}"
+cc_make_depends_ubuntu25="${cc_make_depends}"
 
 cc_command_configure='
 ./configure --quiet
@@ -61,8 +64,14 @@ CXXFLAGS="-O3 -march=native"
 --enable-reduce-exports --without-miniupnpc --without-zmq
 --with-gui=auto
 '
+cc_command_configure_debian12="${cc_command_configure}"
+cc_command_configure_debian13="${cc_command_configure}"
+cc_command_configure_ubuntu25="${cc_command_configure}"
+cc_command_configure_ubuntu24="${cc_command_configure}"
 
-cc_command_configure_ubuntu='
+cc_make_depends_test="bdb boost"
+
+cc_command_configure_test='
 ./configure
 LDFLAGS="-L`pwd`/depends/${cc_archdir}/lib/"
 CPPFLAGS="-I`pwd`/depends/${cc_archdir}/include/"
