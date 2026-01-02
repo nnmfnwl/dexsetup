63498156b2b3a6ada650cfb809b470d186158ab2
no en am fro no wer lnd <134507773+nnmfnwl@users.noreply.github.com>
2026-01-02 11:48:42 +0000

    upt cfg.torbrowser.sh - update tor browser installer version to 15.0.3

--- src/cfg.torbrowser.sh
+++ src/cfg.torbrowser.sh
@@ -9,8 +9,11 @@ cc_git_branch="base-browser-115.10.0esr-13.5-1"
 cc_git_commit_id="ca81f26e66ca397beeb201bc81940b79e3298ebf"
 cc_git_build_bin_file_dir="build"
 
-cc_download_url_x86_64="https://archive.torproject.org/tor-package-archive/torbrowser/14.5.3/tor-browser-linux-x86_64-14.5.3.tar.xz"
-cc_download_sha512sum_x86_64="e223dd1befe023f413abe874dfe8eca4a46252856430da9f9d7728fefa6bcf168c0f83c2a009438fbf58126f1eb4cb80afc5126270ea253e8ef453db8d5f97fe"
+#~ cc_download_url_x86_64="https://archive.torproject.org/tor-package-archive/torbrowser/14.5.3/tor-browser-linux-x86_64-14.5.3.tar.xz"
+#~ cc_download_sha512sum_x86_64="e223dd1befe023f413abe874dfe8eca4a46252856430da9f9d7728fefa6bcf168c0f83c2a009438fbf58126f1eb4cb80afc5126270ea253e8ef453db8d5f97fe"
+
+cc_download_url_x86_64="https://archive.torproject.org/tor-package-archive/torbrowser/15.0.3/tor-browser-linux-x86_64-15.0.3.tar.xz"
+cc_download_sha512sum_x86_64="f26d33e6a8fead0ce7ffbf8bfac594808be2f1b571ce99fddb1b6a39bf03ba7729941d88ede02d0ba0e3dd9d72a4684c4090a4dfe5fcfa981a9a24dc38865947"
 
 cc_download_extracted_bin_files_dir="tor-browser"
 
