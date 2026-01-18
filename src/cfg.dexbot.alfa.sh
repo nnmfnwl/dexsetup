26245e26778317bbd3f14f2f05f1861444ec4f88
no en am fro no wer lnd <134507773+nnmfnwl@users.noreply.github.com>
2026-01-18 09:28:37 +0000

    upt cfg.dexbot.alfa.sh - update version with pricing outage system feature

--- src/cfg.dexbot.alfa.sh
+++ src/cfg.dexbot.alfa.sh
@@ -3,6 +3,6 @@ cc_setup_helper_version="20210827"
 cc_dexbot_install_dir_path_default="dexbot"
 cc_dexbot_git_src_url="https://github.com/nnmfnwl/dexbot.git"
 cc_dexbot_git_branch="main"
-cc_dexbot_git_commit_id="131fab5cb9271d258a874ca09bf7b62157f618ce"
+cc_dexbot_git_commit_id="3b566feda7e6e41c35c94ac955163ac58a0b40af"
 
 cc_dexbot_strategy_template_path_relative="/howto/examples/bot_v2_template.py"
