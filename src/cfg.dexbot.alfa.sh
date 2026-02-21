b0398d0a13917c8bd236a986a2e05fd10d0a8186
no en am fro no wer lnd <134507773+nnmfnwl@users.noreply.github.com>
2026-02-21 23:56:29 +0000

    upt cfg.dexbot.alfa.sh - bump dexbot version

--- src/cfg.dexbot.alfa.sh
+++ src/cfg.dexbot.alfa.sh
@@ -3,6 +3,6 @@ cc_setup_helper_version="20210827"
 cc_dexbot_install_dir_path_default="dexbot"
 cc_dexbot_git_src_url="https://github.com/nnmfnwl/dexbot.git"
 cc_dexbot_git_branch="dev.2026.01.18"
-cc_dexbot_git_commit_id="c3a704af3fe4784b6b22d57fb28f3d6aaff75b0e"
+cc_dexbot_git_commit_id="7f768647965f89379f5c02ca63b77fc47cb4ab5a"
 
 cc_dexbot_strategy_template_path_relative="/howto/examples/bot_v2_template.py"
