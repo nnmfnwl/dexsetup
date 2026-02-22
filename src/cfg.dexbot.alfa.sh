b75bedb37cbc32b07028c64341e0b33d8a9ed89e
no en am fro no wer lnd <134507773+nnmfnwl@users.noreply.github.com>
2026-02-22 16:38:46 +0000

    upt cfg.dexbot.alfa.sh - switch dexbot version to last stable

--- src/cfg.dexbot.alfa.sh
+++ src/cfg.dexbot.alfa.sh
@@ -2,7 +2,7 @@ cc_setup_helper_version="20210827"
 
 cc_dexbot_install_dir_path_default="dexbot"
 cc_dexbot_git_src_url="https://github.com/nnmfnwl/dexbot.git"
-cc_dexbot_git_branch="dev.2026.01.18"
-cc_dexbot_git_commit_id="39536fe0926e278ff9bec1df1c36ee4d243eedb1"
+cc_dexbot_git_branch="main"
+cc_dexbot_git_commit_id="1f44d1bdc8476193336307149b49d7c112de27ba"
 
 cc_dexbot_strategy_template_path_relative="/howto/examples/bot_v2_template.py"
