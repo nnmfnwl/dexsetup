d386179729f8b986e8de9ccc50b5f5e0ee364438
no en am fro no wer lnd <134507773+nnmfnwl@users.noreply.github.com>
2026-04-28 09:33:53 +0000

    upt cfg.cc.blocknet.snode.sh - finalize TOR ONION Hidden-service-v3 for Blocknet service node configuration

--- src/cfg.cc.blocknet.snode.sh
+++ src/cfg.cc.blocknet.snode.sh
@@ -10,14 +10,28 @@ cc_rpcport=41414
 
 # commands will be evaluated and executed line by line to first fail which means return non 0
 cc_command_list_post_profile=(
-'setting variable of torrc config'
-'torrc=/usr/share/tor/tor-service-defaults-torrc'
+'setting torrc config path variable'
+'torcfg=/usr/share/tor/tor-service-defaults-torrc'
+
+'setting snode HSV3 data path variable'
+'hscfg=/var/lib/tor/ohs_blocknet_snode/'
+
+'setting snode HSV3 data path variable'
+'hshostnamecfg=/var/lib/tor/ohs_blocknet_snode/hostname'
 
 'detecting su vs sudo administration command'
-'sudo -v; (test $? != 0) && su_cmd=\"echo \\\"Please enter ROOT password\\\"; su -c\" || su_cmd=\"echo \\\"Please enter ${USER} sudo password\\\"; sudo sh -c\";'
+'sudo -v; (test \$? != 0) && su_cmd=\"echo \\\"Please enter <root> password\\\" && su - -c\" || su_cmd=\"echo \\\"Please enter <\${USER}> sudo password\\\" && sudo sh -c\"'
 
-'administration password needed >> updating system >> installing tor >> setting up snode onion hidden service domain name >> adding domain name into blocknet configuration file >> process could take a minute...'
-'${su_cmd} -c \"apt update && apt install tor && cat ${torrc} | grep -q ohs_blocknet_snode || (echo \\\"HiddenServiceDir /var/lib/tor/ohs_blocknet_snode/\\\" >> ${torrc} && echo \\\"HiddenServiceVersion 3\\\" >> ${torrc} && echo \\\"HiddenServicePort ${cc_port} 127.0.0.1:${cc_port}\\\" >> ${torrc} && service tor@default restart && sleep 6 && ohsdn=\$(cat /var/lib/tor/ohs_blocknet_snode/hostname) && echo \\\"externalip=\${ohsdn}\\\" >> ${cc_main_cfg_path})\"'
+'using variables'
+'
+echo \"torcfg \${torcfg}\";
+echo \"hscfg \${hscfg}\";
+echo \"hshostnamecfg \${hshostnamecfg}\";
+echo \"su_cmd \${su_cmd}\"
+'
+
+'administration password >> updating system >> installing tor >> setting up snode onion hidden service domain name >> could take a minute...'
+'${su_cmd} \"(apt update && apt install tor) && ((cat ${torcfg} | grep -q ohs_blocknet_snode) && (echo \\\"**** HSV3 already configured ****\\\") || (echo \\\"HiddenServiceDir ${hscfg}\\\" >> ${torcfg} && echo \\\"HiddenServiceVersion 3\\\" >> ${torcfg} && echo \\\"HiddenServicePort ${cc_port} 127.0.0.1:${cc_port}\\\" >> ${torcfg} && service tor@default restart && sleep 6)) && ((cat ${cc_main_cfg_path} | grep -q \\\"^externalip=\\\") && (echo -n \\\"# \\\" >> ${cc_main_cfg_path}) && (echo \\\"**** ${cc_main_cfg_path} configuration file MUST be corrected manually ****\\\") || (echo \\\"adding externalip configuration line into ${cc_main_cfg_path}\\\")) && (echo -n \\\"externalip=\\\" >> ${cc_main_cfg_path}) && (cat ${hshostnamecfg} >> ${cc_main_cfg_path}) && (echo \\\"TOR ONION Hidden-service-v3 for Blocknet service node configuration success\\\")\"'
 )
 
 # lines will eval before add
